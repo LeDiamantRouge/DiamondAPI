5fcc41eb39bb56173fa4d188c2502630288a7fa3
LeDiamantRouge <dj.poutchcres@gmail.com>
2023-10-10 20:36:25 +0200

    Remove GamePlay

--- src/main/java/fr/lediamantrouge/diamondapi/DiamondAPI.java
+++ src/main/java/fr/lediamantrouge/diamondapi/DiamondAPI.java
@@ -2,7 +2,6 @@ package fr.lediamantrouge.diamondapi;
 
 import fr.lediamantrouge.diamondapi.chat.ChatManager;
 import fr.lediamantrouge.diamondapi.database.DataBaseManager;
-import fr.lediamantrouge.diamondapi.gameplay.GamePlayManager;
 import fr.lediamantrouge.diamondapi.message.MessageManager;
 import fr.lediamantrouge.diamondapi.scoreboard.ScoreBoardManager;
 import fr.lediamantrouge.diamondapi.tag.TagManager;
@@ -17,7 +16,6 @@ public class DiamondAPI {
     ChatManager chatManager = new ChatManager();
     TagManager tagManager = new TagManager();
     ScoreBoardManager scoreBoardManager = new ScoreBoardManager();
-    GamePlayManager gamePlayManager = new GamePlayManager();
     MessageManager messageManager = new MessageManager();
 
     public static DiamondAPI get() {
